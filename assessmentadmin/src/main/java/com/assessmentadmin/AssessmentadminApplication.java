e56d6ea105deabcb27a806a78e1d2155ff12d4b5
vinodkgupta96 <vinodkgupta.1996@gmail.com>
2019-06-22 14:25:25 +0530

    initial commit for assessmentadmin

---
+++ assessmentadmin/src/main/java/com/assessmentadmin/AssessmentadminApplication.java
@@ -0,0 +1,13 @@
+package com.assessmentadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssessmentadminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssessmentadminApplication.class, args);
+	}
+
+}
---
+++ assessmentadmin/src/main/java/com/assessmentadmin/controller/AsessmentAdminController.java
@@ -0,0 +1,12 @@
+package com.assessmentadmin.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("assessmentadmin")
+public class AsessmentAdminController {
+	
+	
+	
+	
+
+}
